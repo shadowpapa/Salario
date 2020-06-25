ca7f22910aff52daf37999a22a97394af18c1e8f
Shadowpapa <jm_matheus_20@hotmail.com>
2020-06-25 20:49:27 -0300

    Add project files.

---
+++ Salario/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salario
+{
+    class Class1
+    {
+        public string nome;
+        public string salario;
+    }
+}
